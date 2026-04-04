8c9430d382b40088953d6c8c7e7281584e2a3364
Digvijay Patel <dpat19122@gmail.com>
2026-04-05 00:25:29 +0530

    added estimators

---
+++ us_visa/entity/estimator.py
@@ -0,0 +1,51 @@
+import sys
+
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+
+from us_visa.exception import USvisaException
+from us_visa.logger import logging
+
+class TargetValueMapping:
+    def __init__(self):
+        self.Certified: int = 0
+        self.Denied: int = 1
+    
+    def _asdict(self):
+        return self.__dict__
+    
+    def reverse_mapping(self, value: int) -> str:
+        mapping_response = self._asdict()
+        return dict(zip(mapping_response.values(),mapping_response.keys()))
+    
+class USvisaModel:
+    def __init__(self, preprocessing_object: Pipeline, trained_model_object: object):
+        """
+        :param preprocessing_object: Input Object of preprocesser
+        :param trained_model_object: Input Object of trained model 
+        """
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
+
+    def predict(self, df: DataFrame) -> DataFrame:
+        """
+        Function accepts raw inputs and then transformed raw input using preprocessing_object
+        which guarantees that the inputs are in the same format as the training data
+        At last it performs prediction on transformed features
+        """
+        logging.info("Entered predict method of UTruckModel class")
+
+        try:
+            logging.info("Using the trained model to get predictions")
+            transformed_feature = self.preprocessing_object.transform(df)
+
+            logging.info("Used the trained model to get predictions")
+            return self.trained_model_object.predict(transformed_feature)
+        except Exception as e:
+            raise USvisaException(e, sys)
+        
+    def __repr__(self) -> str:
+        return f"{type(self.trained_model_object).__name__}()"
+    
+    def __str__(self) -> str:
+        return f"{type(self.trained_model_object).__name__}()"

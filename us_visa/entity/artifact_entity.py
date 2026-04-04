2e9930774e665ae77b59e9dce164871e1872d01f
Digvijay Patel <dpat19122@gmail.com>
2026-04-04 23:20:33 +0530

    added data transformation artifact

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -10,4 +10,11 @@ class DataValidationArtifact:
     validation_status: bool
     message: str
     drift_report_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+    preprocessed_object_file_path: str
+
     
